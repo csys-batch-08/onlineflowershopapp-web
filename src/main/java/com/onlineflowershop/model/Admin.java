55f22525b423b3c41cc1b31dbc3f9362e13dc0e2
premalathathangaraj <thangarajpremalatha@gmail.com>
2022-02-16 10:36:36 +0530

    updated all files

--- src/main/java/com/onlineflowershop/model/Admin.java
+++ src/main/java/com/onlineflowershop/model/Admin.java
@@ -4,56 +4,24 @@ import java.util.Objects;
 
 public class Admin {
 
-	private String name;
-	private String emailId;
-	private String password;
-
+	private String adminUser;
+	private String adminEmailId;
+	private String adminPassword;
+	
+	
 	public Admin() {
 		super();
-
+		
 	}
-
-	public Admin(String name, String emailId, String password) {
-		super();
-		this.name = name;
-		this.emailId = emailId;
-		this.password = password;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmailId() {
-		return emailId;
-	}
-
-	public void setEmailId(String emailId) {
-		this.emailId = emailId;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
 	@Override
 	public String toString() {
-		return "Admin [name=" + name + ", emailId=" + emailId + ", password=" + password + "]";
+		return "Admin [adminUser=" + adminUser + ", adminEmailId=" + adminEmailId + ", adminPassword=" + adminPassword
+				+ "]";
 	}
-
 	@Override
 	public int hashCode() {
-		return Objects.hash(emailId, name, password);
+		return Objects.hash(adminEmailId, adminPassword, adminUser);
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -63,8 +31,33 @@ public class Admin {
 		if (getClass() != obj.getClass())
 			return false;
 		Admin other = (Admin) obj;
-		return Objects.equals(emailId, other.emailId) && Objects.equals(name, other.name)
-				&& Objects.equals(password, other.password);
+		return Objects.equals(adminEmailId, other.adminEmailId) && Objects.equals(adminPassword, other.adminPassword)
+				&& Objects.equals(adminUser, other.adminUser);
+	}
+	public Admin(String adminUser, String adminEmailId, String adminPassword) {
+		super();
+		this.adminUser = adminUser;
+		this.adminEmailId = adminEmailId;
+		this.adminPassword = adminPassword;
+	}
+	public String getAdminUser() {
+		return adminUser;
+	}
+	public void setAdminUser(String adminUser) {
+		this.adminUser = adminUser;
+	}
+	public String getAdminEmailId() {
+		return adminEmailId;
+	}
+	public void setAdminEmailId(String adminEmailId) {
+		this.adminEmailId = adminEmailId;
+	}
+	public String getAdminPassword() {
+		return adminPassword;
+	}
+	public void setAdminPassword(String adminPassword) {
+		this.adminPassword = adminPassword;
 	}
 
+	
 }
